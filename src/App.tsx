5009bf161e0ec6df78a4d72060d3884457027214
MaxxKalashnikov <max.vanholl@mail.ru>
2024-10-10 21:25:08 +0300

    ttt

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
 const [hello, setHello] = useState<string>("")
   return (
     <div className="hello">
+      <h1>Render test</h1>
       <h2>{hello}</h2>
       <button onClick={()=>setHello("hello world!")}>CLICK ME</button>
       <button onClick={()=>setHello("hello world from the second button!")}>CLICK ME AGAIN</button>
