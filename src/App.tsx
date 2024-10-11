579facaa4f49cc659c45c43ab85ce23164c2ebf8
MaxxKalashnikov <max.vanholl@mail.ru>
2024-10-11 12:41:20 +0300

    test auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ const [hello, setHello] = useState<string>("")
       <h2>{hello}</h2>
       <button onClick={()=>setHello("hello world!")}>CLICK ME</button>
       <button onClick={()=>setHello("hello world from the second button!")}>CLICK ME AGAIN</button>
-      <button onClick={()=>setHello("hi there!")}>CLICK</button>
-      <button onClick={()=>setHello("hi there!")}>CLICK</button>
     </div>  
   )
 }
